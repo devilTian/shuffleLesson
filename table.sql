5cd13bab7223b15e47d8a7d5ba7f44cb8b34bbbc
davidTian <spidertianye@hotmail.com>
2012-06-25 20:49:40 +0800

    sql file for creating db and tables

---
+++ table.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE shuffleLesson;
+
+USE shuffleLesson;
+
+CREATE TABLE datetime(
+    id INT NOT NULL,
+    datetime datetime NOT NULL
+)Engine=InnoDB;
+
+CREATE TABLE book(
+   id INT NOT NULL,
+   name char(100) NOT NULL,
+   PRIMARY KEY(id)
+)Engine=InnoDB;
+
+CREATE TABLE lesson(
+   les_id INT NOT NULL AUTO_INCREMENT,
+   les_num INT NOT NULL,
+   les_name CHAR(100) NOT NULL,
+   les_unit CHAR(50) NULL,
+   les_score INT NULL,
+   les_dateid INT NULL,
+   les_bookid INT NOT NULL,
+   PRIMARY KEY(les_id),
+   FOREIGN KEY(les_bookid) references book(id)
+)Engine=InnoDB;
+
+
