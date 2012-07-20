30cfc1da913574b3a7252ede8fb8c2f1bc6a224a
davidTian <spidertianye@hotmail.com>
2012-07-20 10:28:09 +0800

    1. add doc for unit test 2. re-design the location of every source files

---
+++ deliverable/db/db.php
@@ -0,0 +1,50 @@
+<?php
+    abstract class Base {
+	static $DB;
+	static $stmts = array();
+	
+	function __construct() {
+	    #TODO
+	    $dsn  = 'mysql:host=localhost;dbname=shuffleLesson';
+	    $user = 'root';
+	    $pwd  = '123123';
+
+	    if ( is_null( $dsn ) ) {
+		throw new Exception('No DSN');
+	    }
+	    self::$DB = new PDO( $dsn, $user, $pwd );
+	    self::$DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+
+	function getLastInsertId() {
+	    return self::$DB->lastInsertId();
+	}
+
+	function prepareStatement( $stmt_s ) {
+	    if ( isset( self::$stmts[$stmt_s] ) ) {
+		return self::$stmts[$stmt_s];
+	    }
+	    $stmt_handle = self::$DB->prepare( $stmt_s );
+	    self::$stmts[$stmt_s] = $stmt_handle;
+	    return $stmt_handle;
+	}
+
+	protected function doStatement( $stmt_s, $values_a) {
+	    $sth = $this->prepareStatement( $stmt_s );
+	    $sth->closeCursor();
+	    $db_result = $sth->execute( $values_a );
+	    return $sth;
+	}
+    }
+
+
+    class x extends Base {
+        function a() {
+    	    $sql = 'select * from lesson';
+	    $values = array();
+            return $this->doStatement($sql, $values);
+	}
+    }
+    $x = new x();
+    var_dump($x->a()->fetchColumn(2));
+?>
