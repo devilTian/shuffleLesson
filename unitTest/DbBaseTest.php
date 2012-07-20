6c78ff4181422c267f8f1ff50edd32f4e27c37e1
davidTian <spidertianye@hotmail.com>
2012-07-21 05:21:49 +0800

    add new unit test to DbBase class

--- unitTest/DbBaseTest.php
+++ unitTest/DbBaseTest.php
@@ -1,47 +1,62 @@
 <?php
     require_once(PATH);
 
-    class subTest extends DbBase {}
-   
-    class DbTest extends PHPUnit_Framework_TestCase {
+    class mockPDO extends PDO {
+	function __construct() {}
+    }
+
+    class subTest extends DbBase {
+        function doStatement( $stmt_s, $values_a ) {
+	    return parent::doStatement( $stmt_s, $values_a );
+	}
+    }
+
+    class DbBaseTest extends PHPUnit_Framework_TestCase {
 	
 	private $subTest;
 
 	public function setUp() {
             $this->subTest = new subTest();
+
+            $stub = $this->getMock('MockPDO', array('prepare', 'lastInsertId'));
+	    $stub->expects($this->any())
+	         ->method('prepare')
+	         ->will($this->returnValue(new PDOStatement()));
+	    $stub->expects($this->any())
+	         ->method('lastInsertId')
+	         ->will($this->returnValue(1));
+	    subTest::$DB = $stub;
 	}
 
 	public function testGetLastInsertId() {
 	    $actual_value = $this->subTest->getLastInsertId();
-	    $this->assertLessThanOrEqual($actual_value, 0);
+	    $this->assertEquals($actual_value, 1);
 	}
 
 	public function testPrepareStatement() {
-            $stub = $this->getMock('PDO', array(), array('mysql:host=localhost;dbname=shuffleLesson', 'root', '123123'));
-	    $stub->expects($this->any())
-		 ->method('prepare')
-	         ->will($this->returnValue(new PDOStatement()));
 	    $actual_value = $this->subTest->prepareStatement('select * from book');
 	    $this->assertTrue($actual_value instanceof PDOStatement);
 	}
 
         public function testDoStatement() {
-            $stub_1 = $this->getMock('SubTest');
-	    $stub_1->expects($this->any())
+	    $stub_pdoStatement = $this->getMock('PDOStatement', array('closeCursor', 'execute'));
+	    $stub_pdoStatement->expects($this->any())
+	         	      ->method('closeCursor')
+	         	      ->will($this->returnValue(true));
+	    $stub_pdoStatement->expects($this->any())
+	         	      ->method('execute')
+	         	      ->will($this->onConsecutiveCalls(true, false));
+            $stub = $this->getMock('subTest', array('prepareStatement'));
+	    $stub->expects($this->any())
 	         ->method('prepareStatement')
-	         ->will($this->returnValue(new PDOStatement()));
+	         ->will($this->returnValue($stub_pdoStatement));
 
-            $stub = $this->getMock('PDO', array(), array('mysql:host=localhost;dbname=shuffleLesson', 'root', '123123'));
-	    $stub->expects($this->any())
-		 ->method('closeCursor')
-	         ->will($this->returnValue(TRUE));
-	    $stub->expects($this->any())
-		 ->method('execute')
-	         ->will($this->returnValue(TRUE));
+            $actual_value = $stub->doStatement('select * from book where id=?', array(1));
+	    $this->assertTrue($actual_value instanceof PDOStatement);
 
-	//    $actual_value = $this->subTest->doStatement('select * from book where id=?', array(1));
-	  //  $this->assertTrue($actual_value instanceof PDOStatement);
-	    
+	    // executes a prepared statement unsuccessfully
+	    $this->setExpectedException('DbException');
+            $stub->doStatement('select * from book where id=?', array(1));
 	}
 
 	public function tearDown() {
