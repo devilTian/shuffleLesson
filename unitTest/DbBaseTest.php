b9f4880e9e274b65e0fc172f58f291ae9735fd5b
davidTian <spidertianye@hotmail.com>
2012-07-22 11:50:49 +0800

    finish to write test case for Db class

--- unitTest/DbBaseTest.php
+++ unitTest/DbBaseTest.php
@@ -9,6 +9,9 @@
         function doStatement( $stmt_s, $values_a ) {
 	    return parent::doStatement( $stmt_s, $values_a );
 	}
+        function fetch(PDOStatement $sth, $fetchStyle) {
+            return parent::fetch($sth, $fetchStyle);
+	}
     }
 
     class DbBaseTest extends PHPUnit_Framework_TestCase {
@@ -18,7 +21,7 @@
 	public function setUp() {
             $this->subTest = new subTest();
 
-            $stub = $this->getMock('MockPDO', array('prepare', 'lastInsertId'));
+            $stub = $this->getMock('MockPDO', array('prepare', 'lastInsertId', 'fetch'));
 	    $stub->expects($this->any())
 	         ->method('prepare')
 	         ->will($this->returnValue(new PDOStatement()));
@@ -57,6 +60,15 @@
 	    // executes a prepared statement unsuccessfully
 	    $this->setExpectedException('DbException');
             $stub->doStatement('select * from book where id=?', array(1));
+	}
+
+	public function testFetch() {
+	    $stub = $this->getMock('PDOStatement', array('fetch'));
+	    $stub->expects($this->any())
+	         ->method('fetch')
+	         ->will($this->returnValue(array('name' => 'OneDay')));
+            $actual_value = $this->subTest->fetch($stub, PDO::FETCH_ASSOC);
+	    $this->assertEquals($actual_value['name'], 'OneDay');
 	}
 
 	public function tearDown() {
