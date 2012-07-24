6340c304891003a2c0f9684c32e65a5d8068f206
davidTian <spidertianye@hotmail.com>
2012-07-25 01:33:36 +0800

    add 'getAllBooks' function in book class in db dir modify Indentation

--- deliverable/db/book.php
+++ deliverable/db/book.php
@@ -2,34 +2,38 @@
     require_once(__dir__ . '/db.php');
 
     class Book extends DbBase {
-	static $sql = array(
-	    'insert' => 'INSERT INTO book value(\'\', ?)',
-	    'select' => 'select name from book where id = ?',
-	    'delete' => 'delete from book where id = ?',
-	    'update' => 'update book set state = ? where id = ?'
-	);
+		static $sql = array(
+			'selectAllBooks'=> 'SELECT * FROM book',
+	    	'insert'        => 'INSERT INTO book value(\'\', ?)',
+	 	   	'selectABook'   => 'select name from book where id = ?',
+	 	   	'delete' 	    => 'delete from book where id = ?',
+		    'update'        => 'update book set state = ? where id = ?'
+		);
 	
         function addNewBook( $bookName ) {
-	    $values = array( $bookName );
+	    	$values = array( $bookName );
             $this->doStatement(self::$sql['insert'], $values);
         }
        
         function getBookName( $bookId ) {
-	    $values = array( $bookId );
-            $sth    = $this->doStatement(self::$sql['select'], $values);
-	    return $this->fetch($sth, PDO::FETCH_ASSOC);
-	}
+		    $values = array( $bookId );
+            $sth    = $this->doStatement(self::$sql['selectABook'], $values);
+		    return $this->fetch($sth, PDO::FETCH_ASSOC);
+		}
 
         function deleteABook( $bookId ) {
-	    $values = array( $bookId );
-	    $this->doStatement(self::$sql['delete'], $values);
-	}
+	    	$values = array( $bookId );
+	  	  $this->doStatement(self::$sql['delete'], $values);
+		}
 
-	function updateBookState( $bookId, $state ) {
-	    $values = array( $state, $bookId );
-	    $this->doStatement(self::$sql['update'], $values);
-	} 
+		function updateBookState( $bookId, $state ) {
+		    $values = array( $state, $bookId );
+	    	$this->doStatement(self::$sql['update'], $values);
+		}
+		
+		function getAllBooks() {
+            $sth    = $this->doStatement(self::$sql['selectAllBooks'], array());
+		    return $this->fetchAll($sth, PDO::FETCH_ASSOC);
+		}
     }
-
-
 ?>
--- unitTest/db/bookTest.php
+++ unitTest/db/bookTest.php
@@ -5,55 +5,69 @@
 	
         private $book;
 	
-	public function setUp() {
+		public function setUp() {
             $this->book = new Book();
-	}
+		}
 
-	public function testAddNewBook() {
-	    // add a new book successfully
-	    $stub = $this->getMock('Book', array('doStatement'));
-	    $stub->expects($this->any())
-	         ->method('doStatement')
+		public function testAddNewBook() {
+	    	// add a new book successfully
+		    $stub = $this->getMock('Book', array('doStatement'));
+		    $stub->expects($this->any())
+	    	     ->method('doStatement')
 	         ->with('INSERT INTO book value(\'\', ?)', array('testbook'));
-	    $stub->addNewBook('testbook');
+		    $stub->addNewBook('testbook');
 
- 	    // bookname is null, so throw a Exception
-	    $this->setExpectedException('PDOException');
-	    $this->book->addNewBook(null);
-	}	
+ 		    // bookname is null, so throw a Exception
+	    	$this->setExpectedException('PDOException');
+		    $this->book->addNewBook(null);
+		}	
 
- 	public function testGetBookName() {
-	    $stub = $this->getMock('Book', array('doStatement', 'fetch'));
-	    $stub->expects($this->any())
-	         ->method('doStatement')
-	         ->with('select name from book where id = ?', array(1))
-	         ->will($this->returnValue(new PDOStatement()));
-	    $stub->expects($this->any())
-	         ->method('fetch')
-	         ->will($this->returnValue(array('name' => 'testbook')));
-	    $actual_value   = $stub->getBookName(1);
-	    $expected_value = array( 'name' => 'testbook' ); 
-	    $this->assertEquals($actual_value, $expected_value);
-	}
+	 	public function testGetBookName() {
+		    $stub = $this->getMock('Book', array('doStatement', 'fetch'));
+	        $stub->expects($this->any())
+	        	 ->method('doStatement')
+	        	 ->with('select name from book where id = ?', array(1))
+				 ->will($this->returnValue(new PDOStatement()));
+		    $stub->expects($this->any())
+		         ->method('fetch')
+	    	     ->will($this->returnValue(array('name' => 'testbook')));
+	    	$actual_value   = $stub->getBookName(1);
+		    $expected_value = array( 'name' => 'testbook' ); 
+		    $this->assertEquals($actual_value, $expected_value);
+		}
 
-	public function testDeleteABook() {
-	    $stub = $this->getMock('Book', array('doStatement'));
-	    $stub->expects($this->any())
-	         ->method('doStatement')
-	         ->with('delete from book where id = ?', array(1));
-	    $stub->deleteABook(1);
-	}
+		public function testDeleteABook() {
+		    $stub = $this->getMock('Book', array('doStatement'));
+	    	$stub->expects($this->any())
+	        	 ->method('doStatement')
+		         ->with('delete from book where id = ?', array(1));
+		    $stub->deleteABook(1);
+		}
 
-	public function testUpdateBookState() {
-	    $stub = $this->getMock('Book', array('doStatement'));
-	    $stub->expects($this->any())
-	         ->method('doStatement')
-	         ->with('update book set state = ? where id = ?', array('C', 1));
-	    $stub->updateBookState(1, 'C');
-	}
+		public function testUpdateBookState() {
+	    	$stub = $this->getMock('Book', array('doStatement'));
+		    $stub->expects($this->any())
+		         ->method('doStatement')
+	    	     ->with('update book set state = ? where id = ?', array('C', 1));
+		    $stub->updateBookState(1, 'C');
+		}
 
-	public function tearDown() {
-	    unset($this->book);
-	}
-    } 
+		public function testGetAllBooks() {
+			$stub = $this->getMock('Book', array('doStatement', 'fetchAll'));
+	        $stub->expects($this->any())
+	        	 ->method('doStatement')
+	        	 ->with('SELECT * FROM book', array())
+				 ->will($this->returnValue(new PDOStatement()));
+		    $stub->expects($this->any())
+		         ->method('fetchAll')
+	    	     ->will($this->returnValue(array('name' => 'testbook')));
+	    	$actual_value   = $stub->getAllBooks();
+		    $expected_value = array( 'name' => 'testbook' ); 
+		    $this->assertEquals($actual_value, $expected_value);
+		}
+
+		public function tearDown() {
+		    unset($this->book);
+		}
+	} 
 ?>
